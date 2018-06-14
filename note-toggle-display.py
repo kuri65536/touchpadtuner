221ca99fdb5cc7d5cc6192f233d17a6b1bc1a45c
Shimoda <kuri65536@hotmail.com>
2018-06-15 07:22:45 +0900

    change license to MPL2.0

--- note-toggle-display.py
+++ note-toggle-display.py
@@ -1,9 +1,11 @@
 #! /usr/bin/python3
-"""license::
+"""
+Copyright (c) 2017, shimoda as kuri65536 _dot_ hot mail _dot_ com
+                    ( email address: convert _dot_ to . and joint string )
 
- This Source Code Form is subject to the terms of the Mozilla Public License,
- v.2.0. If a copy of the MPL was not distributed with this file,
- You can obtain one at https://mozilla.org/MPL/2.0/.
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v.2.0. If a copy of the MPL was not distributed with this file,
+You can obtain one at https://mozilla.org/MPL/2.0/.
 """
 import re
 import subprocess as sp
